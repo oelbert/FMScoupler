1ce0c8178ff0f2ecf62e6450e492a2936d3cc8fe
Oliver Elbert <oliver.elbert36@gmail.com>
2024-09-03 14:37:35 -0400

    diff timestep

--- SHiELD/coupler_main.F90
+++ SHiELD/coupler_main.F90
@@ -124,7 +124,7 @@ implicit none
 
 !#######################################################################
 
- !$ser verbatim save_timestep = 4
+ !$ser verbatim save_timestep = 2
 
  call fms_init()
 
