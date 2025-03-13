f857c65e4aeb9082bab82a6f455034929d41ee74
Oliver Elbert <Oliver.Elbert@noaa.gov>
2025-03-13 15:57:48 -0400

    16 should work

--- SHiELD/coupler_main.F90
+++ SHiELD/coupler_main.F90
@@ -124,7 +124,7 @@ implicit none
 
 !#######################################################################
 
- !$ser verbatim save_timestep = 17
+ !$ser verbatim save_timestep = 16
 
  call fms_init()
 
